a2f6b2f2ea1ff3c7b60da5ec2c6a60105bc8f069
Melvin Flores <flores.majano@gmail.com>
2018-07-23 22:24:34 -0600

    Nueva Vists

---
+++ WebApplication1/WebApplication1/Controllers/edadController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class edadController : Controller
+    {
+        // GET: edad
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(int edad)
+        {
+            return View();
+        }
+    }
+}
